77eb451debf62fd48466c36027d12410f02d02f3
isaiasMA94 <isaiasmunoza@gmail.com>
2024-11-25 00:57:45 -0300

    listo

--- curso.py
+++ curso.py
@@ -28,5 +28,95 @@ print(True < False)
 print("\"I'm\"\n\"\"learning\"\"\n\"\"\"Python\"\"\"")
 print("\"estoy\"\n\"\"aprendiendo\"\"\n\"\"\"python\"\"\"")
 #operadores
+#python como una calculadora
 print(2+2)
 print("ello world")
+print(2*3)
+print(12 // 4.5)
+print(2 ** 3)
+print(2. ** 3)
+print(9 % 6 % 2)
+print(2 ** 2 ** 3)
+print(2**3)
+print(2 ** 3.)
+print(2. ** 3)
+print(2. *3.)
+print(2 * 3)
+print(2 * 3.)
+print(2. * 3)
+print(2. * 3.)
+print(6 / 3)
+print(6 / 3.)
+print(6. / 3)
+print(6. / 3.)
+print(6 // 3)
+print(6 // 3.)
+print(6. // 3)
+print(6. // 3.)
+print(6//4)
+print(6.//4)
+print(-6//4)
+print(6.//-4)
+#resto o modulo
+print(14%4)
+print(3*4)
+print(14-12)
+print(12//4.5)
+print(2.0*4.5)
+print(12-9.0)
+#suma
+print(-4+4)
+print(-4+8)
+#resta
+print(-4-4)
+print(4.-8)
+print(-1.1)
+print(+2)
+#jerarquia y enlaces de operadores
+print(9%6%2)
+print(2**2**3)
+print(-3 ** 2)
+print(-2 ** 3)
+print(-(3 ** 2))
+print(2 * 3 % 5)
+print((5 * ((25 % 13) + 100) / (2 * 13)) // 2)
+print((2 ** 4), (2 * 4.), (2 * 4))
+print((-2 / 4), (2 / 4), (2 // 4), (-2 // 4))
+print((2 % -4), (2 % 4), (2 ** 3 ** 2))
+#variables y como utilizarlas
+var=2
+account_balance= 1000.0
+client_name= 'john Doe'
+print(var, account_balance, client_name)
+print(var)
+
+var= "3.8.5"
+print("python version: " + var)
+var=1
+print(var)
+var= var +1
+print(var)
+var = 100
+var = 200 + 300
+print(var)
+a = 3.0
+b = 4.0
+c = (a ** 2 + b ** 2) ** 0.5
+print("c =", c)
+
+a = 3.0
+b = 4.0
+c = (a ** 2 + b ** 2) ** 0.5
+print("c =", c)
+
+john = 3
+mary = 5
+adam = 6
+print( john,mary,adam,  sep=",")
+total_apples=(john + mary + adam)
+print(total_apples)
+x = x * 2
+print(x)
+
+
+      
